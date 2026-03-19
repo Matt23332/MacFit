8e6a649be3da4337e859e19775525ce53f1a0970
Mwenda Muriithi <129791770+Matt23332@users.noreply.github.com>
2026-03-19 17:23:26 +0300

    Creating test account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -19,9 +20,12 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
         ]);
 
+        $adminRole = Role::where('name', 'Admin')->first();
+
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'role_id' => $adminRole->id,
         ]);
     }
 }
